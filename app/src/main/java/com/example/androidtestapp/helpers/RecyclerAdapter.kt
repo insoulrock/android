2782b42ec668aaf85bb28949ff107eeb067d0b84
Pantyukhov Sergey <insoulrock@gmail.com>
2020-11-25 15:35:18 +0300

    cs

--- app/src/main/java/com/example/androidtestapp/helpers/RecyclerAdapter.kt
+++ app/src/main/java/com/example/androidtestapp/helpers/RecyclerAdapter.kt
@@ -3,6 +3,7 @@ package com.example.androidtestapp.helpers
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.graphics.Color
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.layout_recycler_view.view.*
 import com.example.androidtestapp.R
@@ -12,6 +13,8 @@ import kotlin.collections.ArrayList
 class RecyclerAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>()
 {
     private var items: List<TickerModel> = ArrayList()
+    private val colorWinner: Int = Color.parseColor("#33AD2F")
+    private val colorLoser: Int = Color.parseColor("#FF0000")
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         return ViewHolder(
@@ -21,10 +24,27 @@ class RecyclerAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>()
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         var ticker = items[position]
+
+        if(ticker.last == null) return;
+
         holder.itemView.tickerName.setText(ticker.instrument)
         holder.itemView.tickerCourse.setText(String.format("%.10f", ticker.last))
-        var percentChange = ticker.percentChange.toString()
-        holder.itemView.tickerPercentChange.setText("($percentChange%)")
+
+        var percentChange = ticker.percentChange?.toDouble()
+        if(percentChange != null) {
+            var percentChangeDesc = ""
+
+            if (percentChange > 0)
+            {
+                holder.itemView.tickerPercentChange.setTextColor(colorWinner)
+                percentChangeDesc = "+$percentChange%"
+            }
+            else if (percentChange < 0){
+                holder.itemView.tickerPercentChange.setTextColor(colorLoser)
+                percentChangeDesc = "$percentChange%"
+            }
+            holder.itemView.tickerPercentChange.setText(percentChangeDesc)
+        }
     }
 
     override fun getItemCount() = items.size
